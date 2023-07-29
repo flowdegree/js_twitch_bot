c7bbf02fbf16f2ef795a58560cf25431427e7fab
Mohannad Otaibi <mohannad.otaibi@gmail.com>
2023-07-29 14:16:50 +0300

    initial separation from main repo

---
+++ rebuild_docker.sh
@@ -0,0 +1,6 @@
+git pull
+docker stop js_twitch_bot
+docker rm js_twitch_bot
+docker rmi mo9a7i/js_twitch_bot
+docker build -t mo9a7i/js_twitch_bot .
+docker run --name js_twitch_bot -d mo9a7i/js_twitch_bot
